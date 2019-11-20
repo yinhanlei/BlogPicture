aff57e89fe3b36d706757ff05d6d4f2febe9f39b
yhl <1552108403@qq.com>
2019-11-20 18:46:49 +0800

    add photos

--- tool.py
+++ tool.py
@@ -132,7 +132,7 @@ def handle_photo():
             list_info[-1]['arr']['type'].append('image')
     list_info.reverse()  # 翻转
     final_dict = {"list": list_info}
-    with open("E:/hexosite/source/photos/data.json","w", encoding="utf-8") as fp:
+    with io.open("E:/hexosite/source/photos/data.json","w", encoding='utf-8') as fp:
         json.dump(final_dict, fp)
 
 def cut_photo():
